df55526230b7fe70116884008522d4dd50935f33
kossivi <kougblenoukossivi@gmail.com>
2024-05-05 15:16:25 +0200

    Delete old files

--- league/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import standings, add_match, team_detail
-
-urlpatterns = [
-    path('', standings, name='standings'),
-    path('add/', add_match, name='add_match'),
-    path('team/<int:team_id>/', team_detail, name='team_detail')
-]
