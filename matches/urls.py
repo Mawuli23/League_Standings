eb328ab4a7a8e2fd71adf018dd5141e5a1b8fe05
Mawuli <skm.senyo@gmail.com>
2024-05-11 20:21:09 +0200

    modification logique de l'app et front

--- matches/urls.py
+++ matches/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-from .views import index, add_team, list_teams, generate_matches, view_standings
-from .views import list_and_update_matches, team_detail
+from .views import HomePageView, CreateLeagueView, ShowLeagueView, CreateTeamView, GenerateMatchesView
+from .views import MatchListView, ListAndUpdateMatchesView, LeagueStandingsView, DeleteLeagueView, TeamListView
+from .views import AllLeagueStandingsView
 
 urlpatterns = [
-    path("", index, name='index'),
-    path("addteam", add_team, name='add_team'),
-    path("LesTeams", list_teams, name='listTeam'),
-    path('generate_matches/', generate_matches, name='generate_matches'),
-    path('standings/', view_standings, name='view_standings'),
-    path('matches/', list_and_update_matches, name='list_and_update_matches'),
-    path('matches/<int:match_id>/', list_and_update_matches, name='list_and_update_matches'),
-    path('team/<int:team_id>/', team_detail, name='team_detail')
-
+    path("", HomePageView.as_view(), name='index'),
+    path("leagues", CreateLeagueView.as_view(), name='createLeague'),
+    path('league/<int:pk>/delete/', DeleteLeagueView.as_view(), name='deleteLeague'),
+    path("all-leagues", ShowLeagueView.as_view(), name='allLeague'),
+    path('create-team/<int:league_id>/', CreateTeamView.as_view(), name='createTeam'),
+    path('teams/<int:league_id>/allTeams', TeamListView.as_view(), name='teamList'),
+    path('league/<int:league_id>/generate-matches/', GenerateMatchesView.as_view(), name='generateMatches'),
+    path('matches/<int:league_id>/allMatches', MatchListView.as_view(), name='matchList'),
+    path('league/<int:league_id>/update-matches/', ListAndUpdateMatchesView.as_view(), name='updateLeagueMatches'),
+    path('league/<int:league_id>/standings/', LeagueStandingsView.as_view(), name='leagueStandings'),
+    path('all/leagues/standings/', AllLeagueStandingsView.as_view(), name='allLeagueStandings'),
 ]
