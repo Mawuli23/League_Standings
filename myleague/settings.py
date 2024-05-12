d70595ad5748568cdea146d48ca869ac127a83b8
kossivi <kougblenoukossivi@gmail.com>
2024-05-12 13:48:35 +0200

    remove livereload.middleware.LiveReloadScript from MIDDLEWARE

--- myleague/settings.py
+++ myleague/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "livereload.middleware.LiveReloadScript",
+    # "livereload.middleware.LiveReloadScript",
 ]
 
 ROOT_URLCONF = "myleague.urls"
